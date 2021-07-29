efa90d95764a5094c37bc217b1b9591b10cdc5c7
Shubham <shubhamhr07@gmail.com>
2021-07-30 03:29:38 +0530

    docker

---
+++ jobDsl/nodejsdocker.groovy
@@ -0,0 +1,26 @@
+job('NodeJS docker') {
+    scm {
+	    git('git://github.com/shubhamhr07/nodeJenkins.git') {  node -> // is hudson.plugins.git.GitSCM
+            node / gitConfigName('DSL User')
+            node / gitConfigEmail('jenkins-dsl@newtech.academy')
+        }
+    }
+    triggers {
+        scm('H/5 * * * *')
+    }
+    wrappers {
+        nodejs('nodejs') // this is the name of the NodeJS installation in 
+                         // Manage Jenkins -> Configure Tools -> NodeJS Installations -> Name
+    }
+    steps {
+        dockerBuildAndPublish {
+            repositoryName('shubhamhr07/testapp')
+            tag('${GIT_REVISION,length=9}')
+            registryCredentials('dockerhub')
+            forcePull(false)
+            forceTag(false)
+            createFingerprints(false)
+            skipDecorate()
+        }
+    }
+}
